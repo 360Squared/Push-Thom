48009f987e25dfc27263bd4c2815c216832070e5
thom <tpeele@csolutions.nl>
2017-11-23 14:25:37 +0100

    updated poc so it works

--- Week1/poc/Geofencing/app/src/main/java/com/example/thom/geofencing/Constants.java
+++ Week1/poc/Geofencing/app/src/main/java/com/example/thom/geofencing/Constants.java
@@ -35,7 +35,7 @@ final class Constants {
      */
     static final long GEOFENCE_EXPIRATION_IN_MILLISECONDS =
             GEOFENCE_EXPIRATION_IN_HOURS * 60 * 60 * 1000;
-    static final float GEOFENCE_RADIUS_IN_METERS = 20;
+    static final float GEOFENCE_RADIUS_IN_METERS = 100;
 
     /**
      * Map for storing information about airports in the San Francisco bay area.
--- Week1/poc/Geofencing/app/src/main/java/com/example/thom/geofencing/MainActivity.java
+++ Week1/poc/Geofencing/app/src/main/java/com/example/thom/geofencing/MainActivity.java
@@ -5,55 +5,130 @@ import com.google.android.gms.location.Geofence;
 import com.google.android.gms.location.GeofencingClient;
 import com.google.android.gms.location.GeofencingRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
+import android.Manifest;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnCompleteListener<Void> {
     private static final String TAG = MainActivity.class.getSimpleName();
     private static final int REQUEST_PERMISSIONS_REQUEST_CODE = 34;
 
+    private Geofence geofence;
     private GeofencingClient mGeofencingClient;
+    private PendingIntent mGeofencePendingIntent;
+    private static final int LOCATION_REQUEST = 1340;
+
+    private static final String[] LOCATION_PERMS = {
+            Manifest.permission.ACCESS_FINE_LOCATION
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (canAccessLocation()) {
+            Toast.makeText(getApplicationContext(),"Can access location YEAH", Toast.LENGTH_LONG).show();
+            makeGeofence();
+        } else {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(LOCATION_PERMS, LOCATION_REQUEST);
+                if (canAccessLocation()) {
+                    makeGeofence();
+                } else {
+                    Toast.makeText(getApplicationContext(),"Can't access location.", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    }
+
+    private void makeGeofence() {
         mGeofencingClient = LocationServices.getGeofencingClient(this);
 
-        Geofence geofence = new Geofence.Builder()
+        geofence = new Geofence.Builder()
                 // Set the request ID of the geofence. This is a string to identify this geofence.
                 .setRequestId("asntuaoe")
                 // Set the circular region of this geofence. latitude longitude
-                .setCircularRegion(51.9867038,5.9510748, Constants.GEOFENCE_RADIUS_IN_METERS)
+                .setCircularRegion(51.9867038, 5.9510748, Constants.GEOFENCE_RADIUS_IN_METERS)
                 // Set the expiration duration of the geofence. This geofence gets automatically removed after this period of time.
                 .setExpirationDuration(Constants.GEOFENCE_EXPIRATION_IN_MILLISECONDS)
                 // Set the transition types of interest. Alerts are only generated for these transition. We track entry and exit transitions in this sample.
                 .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER | Geofence.GEOFENCE_TRANSITION_EXIT)
                 .build();
+        addGeofences();
+
+        Log.d(TAG, "onCreate: " + geofence.toString());
+    }
 
 
+    private boolean canAccessLocation() {
+        return (hasPermission(Manifest.permission.ACCESS_FINE_LOCATION));
+    }
+
+    private boolean hasPermission(String perm) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            return (PackageManager.PERMISSION_GRANTED == checkSelfPermission(perm));
+        }
+        return false;
+    }
+
+    private PendingIntent getGeofencePendingIntent() {
+        // Reuse the PendingIntent if we already have it.
+        if (mGeofencePendingIntent != null) {
+            return mGeofencePendingIntent;
+        }
+        Intent intent = new Intent(this, GeofenceTransitionsIntentService.class);
+        // We use FLAG_UPDATE_CURRENT so that we get the same pending intent back when
+        // calling addGeofences() and removeGeofences().
+        mGeofencePendingIntent = PendingIntent.getService(this, 0, intent, PendingIntent.
+                FLAG_UPDATE_CURRENT);
+        return mGeofencePendingIntent;
+    }
+
+
+    /**
+     * Adds geofences. This method should be called after the user has granted the location
+     * permission.
+     */
+    @SuppressWarnings("MissingPermission")
+    private void addGeofences() {
+
+        mGeofencingClient.addGeofences(getGeofencingRequest(), getGeofencePendingIntent()).addOnCompleteListener(this);
+    }
+
+    /**
+     * Builds and returns a GeofencingRequest. Specifies the list of geofences to be monitored. Also
+     * specifies how the geofence notifications are initially triggered.
+     */
+    private GeofencingRequest getGeofencingRequest() {
         GeofencingRequest.Builder builder = new GeofencingRequest.Builder();
+
+        // The INITIAL_TRIGGER_ENTER flag indicates that geofencing service should trigger a
+        // GEOFENCE_TRANSITION_ENTER notification when the geofence is added and if the device
+        // is already inside that geofence.
         builder.setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER);
 
         // Add the geofences to be monitored by geofencing service.
         builder.addGeofence(geofence);
 
         // Return a GeofencingRequest.
-        builder.build();
-
-        Log.d(TAG, "onCreate: " + geofence.toString());
+        return builder.build();
     }
 
     /**
      * Callback received when a permissions request has been completed.
      */
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
-                                           @NonNull int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         Log.i(TAG, "onRequestPermissionResult");
         if (requestCode == REQUEST_PERMISSIONS_REQUEST_CODE) {
             if (grantResults.length <= 0) {
@@ -71,5 +146,10 @@ public class MainActivity extends AppCompatActivity {
 
             }
         }
+    }
+
+    @Override
+    public void onComplete(@NonNull Task<Void> task) {
+
     }
 }
